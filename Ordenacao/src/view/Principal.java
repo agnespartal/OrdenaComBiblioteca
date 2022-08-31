6107d8383633033028b9ef5652895e8782de23ce
Agnes <95083660+agnespartal@users.noreply.github.com>
2022-08-31 19:02:26 -0300

    Add files via upload

---
+++ Ordenacao/src/view/Principal.java
@@ -0,0 +1,16 @@
+package view;
+
+import controller.Ordenacao;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Ordenacao ord = new Ordenacao();
+		int[] vetor = {89, 54, 18, 71, 93, 45, 19, 14, 99, 2, 27, 3, 58, 91, 75};
+		System.out.println("Bubble sort");
+		ord.ordenacaoBubble(vetor);
+		System.out.println("\n\nMerge sort");
+		ord.ordenacaoMergeSort(vetor);
+	}
+
+}
